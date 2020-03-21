8b94845e10d1cdbefa33e7fc19755877831a2d27
George Travis <wewerelikesharks@gmail.com>
2020-03-21 16:13:52 -0700

    html routes ok

---
+++ server.js
@@ -0,0 +1,26 @@
+`use strict`;
+
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+
+const PORT = process.env.PORT || 3000;
+
+const db = require(`./models`);
+
+const apiRoutes = require(`./routes/api-routes`);
+const htmlRoutes = require(`./routes/html-routes`);
+
+const app = express();
+
+app.use(express.static(`public`));
+app.use(logger(`dev`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
